65f97291fdd8fdd77688e583b23219696b3dbe0b
Will Haslett <will.haslett@gmail.com>
2018-03-03 22:23:27 -0500

    Progress on deidentify.rb

--- lib/deidentify.rb
+++ lib/deidentify.rb
@@ -6,9 +6,45 @@ def deidentify!
     raise ArgumentError.new("Refusing to deidentify database named #{TMP_DB}")
     return
   end
-
   # Load project-specfic deidentification config
-  settings = YAML.load_file(pf 'settings.yml').remove!('ignore')
+  fields = YAML.load_file(pf 'fields.yml').remove!('ignore')
+  fields.each do |f|
+    puts "      #{f['name']} => #{f['output']}".blue
+    alter(field)
+  end
+end
 
-  cputs settings.inspect
+def alter(field)
+  primary_keys(field).each do |primary_key|
+    perform_update(field, primary_key)
+  end 
+end
+
+def primary_keys(field)
+  str = "UPDATE #{field['table']} "
+  str += "#{where_and(str)} #{field} IS NOT NULL " if field['leave_null']
+  field['select_on'].each do |column, value|
+    str += "#{where_and(str)} #{column]} = #{value} "
+  end
+  str += "SET #{field['column']} = #{out_val(field['output'])}"
+end
+
+# def primary_keys(field)
+#   str = "UPDATE #{field['table']} "
+#   str += "#{where_and(str)} #{field} IS NOT NULL " if field['leave_null']
+#   field['select_on'].each do |column, value|
+#     str += "#{where_and(str)} #{column]} = #{value} "
+#   end
+#   str += "SET #{field['column']} = #{out_val(field['output'])}"
+# end
+
+def out_val(type)
+  if type == 'random' 
+    SecureRandom.hex[0..10]
+  end
+end
+
+# Starts a WHERE clause unless we're already in one, then uses AND
+def where_and(str)
+  str.include?('WHERE') ? 'AND' : 'WHERE'
 end
