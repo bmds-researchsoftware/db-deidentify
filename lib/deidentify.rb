e77df9fe54afe054cb4aedbcd66620881dbaf36a
Will Haslett <will.haslett@gmail.com>
2018-03-04 17:43:34 -0500

    cast much-used hash to openstruct. This was rewarding

--- lib/deidentify.rb
+++ lib/deidentify.rb
@@ -6,10 +6,14 @@ def deidentify!
     raise ArgumentError.new("Refusing to deidentify database named #{TMP_DB}")
     return
   end
-  # Load project-specfic deidentification config
+  # Load project-specfic deidentification config into a struct
   fields = YAML.load_file(pf 'fields.yml').remove!('ignore')
   # Iterate of each field (top-level map) in fields.yml and alter records
-  fields.each {|field| alter(field)}
+  fields.each do |field|
+    # Dot notation is more fun than hash notation
+    field = OpenStruct.new(field).freeze
+    alter(field)
+  end
 end
 
 def alter(field)
@@ -18,24 +22,24 @@ def alter(field)
 end
 
 def primary_keys(field)
-  sql = "SELECT #{field['primary_key_col']} from #{field['table']} "
-  sql += "#{where_and(sql)} #{field['column']} IS NOT NULL " if field['leave_null']
-  field['select_on'].each_pair do |column, value|
+  sql = "SELECT #{field.primary_key_col} from #{field.table} "
+  sql += "#{where_and(sql)} #{field.column} IS NOT NULL " if field.leave_null
+  field.select_on.each_pair do |column, value|
     sql += "#{where_and(sql)} #{column} = #{value} "
   end
-  sql += "ORDER BY #{field['primary_key_col']};"
-  # sql += "ORDER BY #{field['primary_key_col']} ASC LIMIT 500;"
+  sql += "ORDER BY #{field.primary_key_col};"
+  # sql += "ORDER BY #{field.primary_key_col} ASC LIMIT 500;"
   execute(sql).split("\n")
 end
 
 def build_sql(field)
   statement_sql = ''
   keys = primary_keys(field)
-  puts "      Altering #{keys.length} records for: #{field['name']} => #{field['output']}".blue
+  puts "      Altering #{keys.length} records for: #{field.name} => #{field.output}".blue
   keys.each do |primary_key|
-    record_sql = "UPDATE #{field['table']} "
-    record_sql += "SET #{field['column']} = #{out_val(field)} "
-    record_sql += "#{where_and(record_sql)} #{field['primary_key_col']} = #{primary_key};\n"
+    record_sql = "UPDATE #{field.table} "
+    record_sql += "SET #{field.column} = #{out_val(field)} "
+    record_sql += "#{where_and(record_sql)} #{field.primary_key_col} = #{primary_key};\n"
     statement_sql += record_sql
   end 
   # puts statement_sql
@@ -43,7 +47,7 @@ def build_sql(field)
 end
 
 def out_val(field)
-  if field['output'] == 'random' 
+  if field.output == 'random' 
     "'#{SecureRandom.hex[1..10]}'"
   end
 end
